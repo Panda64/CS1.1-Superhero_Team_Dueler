1fca65dae8e1a3580ddcd4c12502135fe1594604
Panda64 <JaylenSchelb@yahoo.com>
2020-11-22 17:28:27 -0800

    Completed Tutorial

--- hero.py
+++ hero.py
@@ -1,14 +1,25 @@
 import random
+from ability import Ability
+from armor import Armor
+from weapon import Weapon
 
 class Hero:
+
     # We want our hero to have a default "starting_health",
     # so we can set that in the function header.
     def __init__(self, name, starting_health=100):
         '''Instance properties:
-          name: String
-          starting_health: Integer
-          current_health: Integer
+            abilities: List
+            armors: List
+            name: String
+            starting_health: Integer
+            current_health: Integer
         '''
+
+        # abilities and armors don't have starting values,
+        # and are set to empty lists on initialization
+        self.abilities = list()
+        self.armors = list()
         # we know the name of our hero, so we assign it here
         self.name = name
         # similarly, our starting health is passed in, just like name
@@ -17,17 +28,104 @@ class Hero:
         # always the same as their starting health (no damage taken yet!)
         self.current_health = starting_health
 
-    def fight(self, opponent):
-        ''' Current Hero will take turns fighting the opponent hero passed in. '''
-        fighters = [self.name, opponent.name]
-        winner = random.choice(fighters)
+        self.deaths = 0
+        self.kills = 0
 
-        print(f"{winner} won!")
+    def fight(self, opponent):  
+        ''' Current Hero will take turns fighting the opponent hero passed in.
+        '''
+        if not self.abilities or not opponent.abilities:
+            print("Draw")
+        else:
+            while True:
+                opponent.take_damage(self.attack())
+                if opponent.is_alive():
+                   None
+                else:
+                    print(f"{self.name} won!")
+                    self.add_kill(1)
+                    opponent.add_death(1)
+                    break
+
+                self.take_damage(opponent.attack())
+                if self.is_alive():
+                   None
+                else:
+                    print(f"{opponent.name} won!")
+                    self.add_death(1)
+                    opponent.add_kill(1)
+                    break
+
+
+    def add_ability(self, ability):
+        ''' Add ability to abilities list '''
+
+        # We use the append method to add ability objects to our list.
+        self.abilities.append(ability)
+
+    def attack(self):
+        '''Calculate the total damage from all ability attacks.
+            return: total_damage:Int
+        '''
+
+        # start our total out at 0
+        total_damage = 0
+        # loop through all of our hero's abilities
+        for ability in self.abilities:
+            # add the damage of each attack to our running total
+            total_damage += ability.attack()
+        # return the total damage
+        return total_damage
+
+    def add_armor(self, armor):
+        '''Add armor to self.armors
+            Armor: Armor Object
+        '''
+        self.armors.append(armor)
+
+    def defend(self, damage_amt):
+        '''Calculate the total block amount from all armor blocks.
+            return: total_block:Int
+        '''
+        total_block = 0
+        for armor in self.armors:
+            total_block += armor.block()
+        
+        if total_block >= damage_amt:
+            return damage_amt
+        else:
+            return total_block
+
+    def take_damage(self, damage):
+        '''Updates self.current_health to reflect the damage minus the defense.
+        '''
+        damage_taken = damage - self.defend(damage)
+        self.current_health = self.current_health - damage_taken
+
+    def is_alive(self):  
+        '''Return True or False depending on whether the hero is alive or not.
+        '''
+        if self.current_health <= 0:
+            return False
+        else:
+            return True
+
+    def add_weapon(self, weapon):
+        '''Add weapon to self.abilities'''
+        self.abilities.append(weapon)
+
+    def add_kill(self, num_kills):
+        ''' Update self.kills by num_kills amount'''
+        self.kills += num_kills
+
+    def add_death(self, num_deaths):
+        ''' Update deaths with num_deaths'''
+        self.deaths += num_deaths
     
 if __name__ == "__main__":
     # If you run this file from the terminal
     # this block is executed.
-    hero1 = Hero("Wonder Woman")
-    hero2 = Hero("Dumbledore")
-
-    hero1.fight(hero2)
+    hero = Hero("Wonder Woman")
+    weapon = Weapon("Lasso of Truth", 90)
+    hero.add_weapon(weapon)
+    print(hero.attack())
